ed0efa13a31dfc521703ca2e7977fcd00d2f9a86
Ayyan <ayyansaddiqui420@gmail.com>
2023-10-20 02:04:49 +0500

    adding all pld data now

--- pld_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PldAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pld_app'
--- pld_app/forms.py
+++
@@ -1,7 +0,0 @@
-# from django import forms
-# from .models import Plants
-
-# class upload_plants_images(forms.ModelForm):
-#     class Meta:
-#         model = Plants
-#         fields = ['plant_leaf_image']
